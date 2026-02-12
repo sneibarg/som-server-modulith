af9ebed0fb54c76fe9cec5332dfc21c81a8d82db
Scott Neibarger <scott.neibarger@gmail.com>
2026-02-11 22:51:57 -0500

    Removing unnecessary config class and moving annotation to the App.

--- src/main/java/org/springy/som/modulith/SomServerApplication.java
+++ src/main/java/org/springy/som/modulith/SomServerApplication.java
@@ -3,7 +3,9 @@ package org.springy.som.modulith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.modulith.Modulithic;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @Modulithic
 @SpringBootApplication
 public class SomServerApplication {
